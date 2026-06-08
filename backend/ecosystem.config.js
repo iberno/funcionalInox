482cb350b8b7ad6935e76f558e72fba6aa0e7b48
iberno <iberno@outlook.com>
2026-06-08 19:09:37 -0300

    fix: remove hardcoded cwd from pm2 config

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -2,13 +2,12 @@ module.exports = {
   apps: [{
     name: 'funcional-inox',
     script: 'dist/src/main.js',
-    cwd: '/home/iberno/Projetos/FuncionalInox/backend',
     instances: 1,
     exec_mode: 'fork',
+    env_file: '.env',
     env: {
       NODE_ENV: 'production',
       PORT: 3001,
     },
-    env_file: '.env',
   }],
 }
